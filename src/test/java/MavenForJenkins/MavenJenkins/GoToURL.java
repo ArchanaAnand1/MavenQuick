57c34490372efe68f490dcc8c1dba6ff3f726464
Archana Anand <archana.anand53@gmail.com>
2021-04-02 01:00:34 +0530

    done

--- src/test/java/MavenForJenkins/MavenJenkins/GoToURL.java
+++ src/test/java/MavenForJenkins/MavenJenkins/GoToURL.java
@@ -1,8 +1,15 @@
 package MavenForJenkins.MavenJenkins;
 
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;;
 
 public class GoToURL {
 
@@ -10,10 +17,13 @@ public class GoToURL {
 	public void test() {
 		// TODO Auto-generated method stub
 		
-		System.setProperty("webdriver.gecko.driver","D:\\Java Software\\gecko driver\\geckodriver.exe");
-		WebDriver driver = new FirefoxDriver();
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
 		driver.get("https://reqres.in/");
-
+		driver.manage().window().maximize();
+		driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
+		driver.findElements(By.tagName("a")).stream().map(e->e.getText()).collect(Collectors.toList())
+		.forEach(e->System.out.println(e));
 	}
 
 }
